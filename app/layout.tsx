ee8d2502bd293f1cfbf6f81816483581eefccd53
Sarah Glenn <111394151+skglenn07@users.noreply.github.com>
2025-03-04 11:54:44 -0500

    added ResuMe.ico directly to metadata in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: PAGE_TITLE,
   description: PAGE_DESCRIPTION,
+  icons: "/ResuMe_icon.ico",
 };
 
 export default function RootLayout({
